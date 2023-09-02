004b35be6160f9db99e024db229b48442dc7fc65
IRMuhammadjon <mirahmatullayev@gmail.com>
2023-09-02 15:52:17 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Hello, World!");
+string str = "telefOn";
+int x = Convert.ToInt32(Console.ReadLine());
+if (x > str.Length)
+{
+    str = str.ToUpper();
+}
+else
+{
+    str = str.ToLower();
+}
+Console.WriteLine(str);
